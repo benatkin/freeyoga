627df984fafec09e050c54d1448e5ceea32e01e4
Benjamin Atkin <ben@benatkin.com>
2018-02-12 23:59:11 -0800

    set up material UI

--- components/head.js
+++ components/head.js
@@ -24,6 +24,7 @@ const Head = (props) => (
     <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
   </NextHead>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -1,84 +1,48 @@
+import {Component} from 'react'
+import {withStyles} from 'material-ui/styles'
 import Link from 'next/link'
 import Head from '../components/head'
-import Nav from '../components/nav'
+import Reboot from 'material-ui/Reboot'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
+import IconButton from 'material-ui/IconButton'
+import MenuIcon from 'material-ui-icons/Menu'
+import Button from 'material-ui/Button'
 
-export default () => (
-  <div>
-    <Head title="Home" />
-    <Nav />
+const styles = {
+  content: {
+    padding: '10px'
+  }
+}
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next!</h1>
-      <p className="description">To get started, edit <code>pages/index.js</code> and save to reload.</p>
+class Home extends Component {
+  render() {
+    const {classes} = this.props
+    return (
+      <div>
+        <Head title="Home" />
+        <Reboot />
 
-      <div className="row">
-        <Link href="https://github.com/zeit/next.js#getting-started">
-          <a className="card">
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next on Github and in their examples</p>
-          </a>
-        </Link>
-        <Link href="https://open.segment.com/create-next-app">
-          <a className="card">
-            <h3>Examples &rarr;</h3>
-            <p>
-              Find other example boilerplates on the <code>create-next-app</code> site
-            </p>
-          </a>
-        </Link>
-        <Link href="https://github.com/segmentio/create-next-app">
-          <a className="card">
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it</p>
-          </a>
-        </Link>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit">
+              Free Yoga
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <div className={classes.content}>
+          <Button variant="raised" color="primary">
+            Hello World
+          </Button>
+        </div>
       </div>
-    </div>
+    )
+  }
+}
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title, .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9B9B9B;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+export default withStyles(styles)(Home)
