faf141aa2ef32e28d88599180fe55d27f29dfc29
Benjamin Atkin <ben@benatkin.com>
2018-03-09 13:42:30 -0500

    add subheading

--- components/profile.js
+++ components/profile.js
@@ -4,6 +4,7 @@ import Typography from 'material-ui/Typography';
 export default ({classes, name, title, image, facebook, linkedin, twitter}) => (
   <Paper>
     <img className={classes.profileImage} src={`/static/images/team/${image}`} />
-    <Typography variant="title" style={{paddingBottom: '5px'}}>{name}</Typography>
+    <Typography variant="title">{name}</Typography>
+    <Typography variant="subheading">{title}</Typography>
   </Paper>
 )
